32926e7608c3d9ec5f0bb7258bffe71f5ec5de6c
Abdikalolo <abdizeabdi18@gmail.com>
2026-08-02 23:16:44 +0300

    debugged on projectcard

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,14 +1,11 @@
-function ProjectCard(){
+function ProjectCard({ title, description }){
     return(
         <div className="card mb-3">
             <div className="card-body">
-                <h5 className="card-title">Project Title</h5>
-                <p className="card-text">
-                    Description of the Project
-                </p>
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
             </div>
         </div>
-
     );
 }
 export default ProjectCard;
