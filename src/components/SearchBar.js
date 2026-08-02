00fdca910ecc834f892ff4e62ec55038ff89d514
Abdikalolo <abdizeabdi18@gmail.com>
2026-08-02 21:56:28 +0300

    created a search bar area for searching projects

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+function SearchBar(){
+    return(
+        <div className="mb-4">
+            <input 
+                 type="text"
+                 className="form-control"
+                 placeholder="Search Projects"
+            ></input>
+        </div>
+    );
+}
+export default SearchBar;
