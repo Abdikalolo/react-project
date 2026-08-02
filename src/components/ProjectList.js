536b2cd049cd9ff220da79ff9e85e4bce697a961
Abdikalolo <abdizeabdi18@gmail.com>
2026-08-02 22:07:55 +0300

    imported project card to project list and created a function for the project list

---
+++ src/components/ProjectList.js
@@ -0,0 +1,12 @@
+import ProjectCard from "./ProjectCard";
+
+function ProjectList(){
+    return (
+        <div>
+            <ProjectCard/>
+            <ProjectCard/>
+            <ProjectCard/>
+        </div>
+    );
+}
+export default ProjectList;
