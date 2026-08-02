5b2306ad85eda78b3abe3aa76e6e9c5f2c4d6ca6
Abdikalolo <abdizeabdi18@gmail.com>
2026-08-02 22:55:36 +0300

    displayed the project cards using an array in appjs

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@ import Header from './components/Header';
 import AddProjectForm from './components/AddProjectForm';
 import SearchBar from './components/SearchBar';
 import ProjectList from './components/ProjectList';
+import { useState } from 'react';
 
 function App (){
+  const [projects, setProjects] = useState([{
+    id: 1,
+    title: "Portfolio Website",
+    description:"A personal portfolio built with react."
+  },
+  {
+    id: 2,
+    title: "Weather App",
+    description: "Weather app that displays weather using an API."
+  },
+  {
+    id: 3,
+    title: "To Do App",
+    description: "An App that manages daily tasks",
+  }]);
+
+  
   return (
     <div className='App'>
       <Header/>
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,14 @@
+function ProjectCard(){
+    return(
+        <div className="card mb-3">
+            <div className="card-body">
+                <h5 className="card-title">Project Title</h5>
+                <p className="card-text">
+                    Description of the Project
+                </p>
+            </div>
+        </div>
+
+    );
+}
+export default ProjectCard;
