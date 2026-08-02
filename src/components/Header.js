d86d578a104133b82b205e913f55f3d3b768937d
Abdikalolo <abdizeabdi18@gmail.com>
2026-08-03 00:37:14 +0300

    updated headerjs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 function Header (){
     return (
-        <header className="bg-dark text-white text-center p-3 mb-4">
-            <h2>Personal Project Showcase App</h2>
-        </header>
+         <header className="bg-primary text-white text-center py-4">
+            <h1>Personal Project Showcase</h1>
+            <p className="mb-o">Manage and showcase your personal projects</p>
+         </header>
     );
 }
 export default Header;
