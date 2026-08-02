a70c36b1678c788db7c2720949b649a600e73cc1
Abdikalolo <abdizeabdi18@gmail.com>
2026-08-02 21:38:41 +0300

    created a function that adds project form

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+function Header (){
+    return (
+        <header className="bg-dark text-white text-center p-3 mb-4">
+            <h2>Personal Project Showcase App</h2>
+        </header>
+    );
+}
+export default Header;
